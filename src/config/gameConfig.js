2fa07d08b481a1b152d9be878d5352e74cdf96c1
shreyansh2k5 <119129251+shreyansh2k5@users.noreply.github.com>
2025-07-05 00:13:42 +0530

    Create gameConfig.js

---
+++ src/config/gameConfig.js
@@ -0,0 +1,22 @@
+// src/config/gameConfig.js
+
+module.exports = {
+    // Coin System Defaults
+    DEFAULT_BALANCE: 10000,
+
+    // Daily Command
+    DAILY_REWARD: 5000,
+    DAILY_COOLDOWN_MS: 24 * 60 * 60 * 1000, // 24 hours
+
+    // Beg Command
+    MIN_BEG_REWARD: 1,
+    MAX_BEG_REWARD: 1000,
+    BEG_COOLDOWN_MS: 5 * 60 * 1000, // 5 minutes
+
+    // Flip Command
+    FLIP_WIN_CHANCE: 0.5, // 50% chance
+
+    // Roll Command
+    ROLL_WIN_MULTIPLIER: 6, // Win amount is bet * (multiplier - 1)
+    ROLL_WIN_CONDITION: 6, // Roll a 6 to win
+};
