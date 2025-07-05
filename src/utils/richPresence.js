fa08b45c7c5e42abf02f78bbb61438395f12cfa0
shreyansh2k5 <119129251+shreyansh2k5@users.noreply.github.com>
2025-07-05 11:28:13 +0530

    Update richPresence.js

--- src/utils/richPresence.js
+++ src/utils/richPresence.js
@@ -4,11 +4,11 @@ const { ActivityType } = require('discord.js');
 /**
  * Sets the bot's activity (rich presence) on Discord.
  * @param {import('discord.js').ClientUser} clientUser The ClientUser object from client.user.
- * @param {string} activityName The name of the activity (e.g., "with coins").
- * @param {ActivityType} activityType The type of activity (e.g., ActivityType.Playing).
+ * @param {string} [activityName='type $help to start'] Optional: The name of the activity. Defaults to 'type $help to start'.
+ * @param {ActivityType} [activityType=ActivityType.Playing] Optional: The type of activity. Defaults to ActivityType.Playing.
  * @param {string} [url] Optional URL for streaming activity type.
  */
-function setBotActivity(clientUser, activityName, activityType, url = null) {
+function setBotActivity(clientUser, activityName = 'type $help to start', activityType = ActivityType.Playing, url = null) {
     const options = { type: activityType };
     if (url && activityType === ActivityType.Streaming) {
         options.url = url;
