f251afddc3e97cff359e97381bdef734cbdf4b60
shreyansh2k5 <119129251+shreyansh2k5@users.noreply.github.com>
2025-07-05 10:58:27 +0530

    Update roll.js

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -56,6 +56,7 @@ module.exports = (coinManager) => ({
     },
 
     async slashExecute(interaction) {
+        await interaction.deferReply({ ephemeral: false });
         const userId = interaction.user.id;
         const username = interaction.user.username;
         const amount = interaction.options.getInteger('amount');
